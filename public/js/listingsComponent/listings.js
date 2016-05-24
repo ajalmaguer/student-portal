3ed9b52f77d7fae74888a5962f4ff83834b37694
AJ Almaguer <ajalmaguer@gmail.com>
2016-05-24 09:56:13 -0700

    can display dummy listings data in a nice format. now need to include ngResource to pull from the backend

--- public/js/listingsComponent/listings.js
+++ public/js/listingsComponent/listings.js
@@ -1,12 +1,7 @@
-var blah = [];
-
 (function () {
   angular
     .module("student-portal")
     .component("listings", {
-      // template: '<div ng-repeat="listing in $ctrl.listings"\n' +
-      //     '<a> {{listing.rent}}</a>\n' +
-      //   '</div>',
       templateUrl: "js/listingsComponent/listings.html",
       controller: ListingsController
     })
@@ -21,47 +16,33 @@ var blah = [];
 
     vm.listings = [
       {
-        rent: 1
+        rent: 1,
+        imageUrl: "https://a1.muscache.com/im/pictures/4b9291ff-8226-4767-9c9b-90c713b7c385.jpg?aki_policy=x_medium",
+        title: "Super Central Santa Monica Stay",
+        roomType: "Private Room",
+        hostImgUrl: "https://a1.muscache.com/im/pictures/1f06226c-28e8-4883-96d8-6ed851f91b30.jpg?aki_policy=profile_medium"
       },
       {
-        rent: 2
+        rent: 2,
+        imageUrl: "https://a2.muscache.com/im/pictures/cfd762e7-fcca-4148-a68e-61ab5cd39d9e.jpg?aki_policy=x_medium",
+        title: "Private room on second floor house",
+        roomType: "Private Room",
+        hostImgUrl: "https://a0.muscache.com/im/pictures/e4a37c9a-25cb-4840-9aaa-ce33f6ed9bba.jpg?aki_policy=profile_medium"
       },
       {
-        rent: 3
+        rent: 3,
+        imageUrl: "https://a2.muscache.com/im/pictures/90ad9834-795a-4107-b5eb-9ddcb50ee9c7.jpg?aki_policy=x_medium",
+        title: "Value Deal!",
+        roomType: "Shared Room",
+        hostImgUrl: "https://a0.muscache.com/im/pictures/e4a37c9a-25cb-4840-9aaa-ce33f6ed9bba.jpg?aki_policy=profile_medium"
       },
       {
-        rent: 4
+        rent: 4,
+        imageUrl: "https://a2.muscache.com/im/pictures/972952e3-5235-46c8-8417-c8873a70078f.jpg?aki_policy=x_medium",
+        title: "Convenient location and best chinese restaurants",
+        roomType: "Shared Room",
+        hostImgUrl: "https://a0.muscache.com/im/pictures/643a2786-da1d-4553-89d0-546a3bd922c1.jpg?aki_policy=profile_medium"
       }]
-
-    //   {
-    //     imageUrls:    ["https://a1.muscache.com/im/pictures/4b9291ff-8226-4767-9c9b-90c713b7c385.jpg?aki_policy=x_medium", "https://a2.muscache.com/im/pictures/84653bf9-f642-4820-adf8-a7d781d7f012.jpg?aki_policy=x_medium"],
-    //     rent:        51,
-    //     title:        "Super Central Santa Monica Stay",
-    //     description:  "Private Room",
-    //     hostImgUrl:   "https://a1.muscache.com/im/pictures/1f06226c-28e8-4883-96d8-6ed851f91b30.jpg?aki_policy=profile_medium"
-    //   },
-    //   {
-    //     imageUrls:    ["https://a2.muscache.com/im/pictures/cfd762e7-fcca-4148-a68e-61ab5cd39d9e.jpg?aki_policy=x_medium", "https://a2.muscache.com/im/pictures/c87082f2-1c9c-4215-a01c-44ec37f65f52.jpg?aki_policy=x_medium"],
-    //     price:        41,
-    //     title:        "Private room on second floor house",
-    //     description:  "Private Room",
-    //     hostImgUrl:   "https://a0.muscache.com/im/pictures/e4a37c9a-25cb-4840-9aaa-ce33f6ed9bba.jpg?aki_policy=profile_medium"
-    //   },
-    //   {
-    //     imageUrls:    ["https://a2.muscache.com/im/pictures/90ad9834-795a-4107-b5eb-9ddcb50ee9c7.jpg?aki_policy=x_medium"],
-    //     price:        45,
-    //     title:        "Value Deal!",
-    //     description:  "Shared Room",
-    //     hostImgUrl:   "carlos.png"
-    //   },
-    //   {
-    //     imageUrls:    ["https://a2.muscache.com/im/pictures/53b8d507-6205-45d8-a582-a27a3bb620e9.jpg?aki_policy=x_medium"],
-    //     price:        59,
-    //     title:        "Convenient location and best chinese restaurants",
-    //     description:  "Shared Room",
-    //     hostImgUrl:   "carlos.png"
-    //   }
-    // ]
 
     vm.$routerOnActivate = function () {
       console.log("this controller was activeated")
