9e40f50ab2cf8ec3ba6ae4911a0727c967d05511
AJ Almaguer <ajalmaguer@gmail.com>
2016-05-24 18:04:48 -0700

    authenitcation sign in and sign up done

--- public/js/app.js
+++ public/js/app.js
@@ -18,9 +18,14 @@
     })
     .component("signin", {
       templateUrl: "js/auth/signin.html",
+      controller:  SignInController,
+      bindings: { $router: '<' }
+
     })
 
     NavbarController.$inject = ["$log", "authService"];
+    SignInController.$inject = ["$log", "authService", "userService"];
+
 
     function NavbarController($log, authService) {
       var vm = this;
@@ -28,5 +33,56 @@
       vm.authService = authService;
 
       $log.info("NavbarController loaded!");
+    }
+
+
+    function SignInController($log, authService, userService) {
+      var vm = this;
+
+      // BINDINGS
+      vm.signUp = {};
+      vm.submitSignUp = submitSignUp;
+      vm.logIn = {};
+      vm.submitLogIn = submitLogIn;
+      vm.conflict = false;
+
+      // FUNCTIONS
+      function submitSignUp() {
+        userService
+          .create(vm.signUp)
+          .then(function(res) {
+            return authService.logIn(vm.signUp);
+          })
+          .then(
+            // on success
+            function(decodedToken) {
+              $log.info('Logged in!', decodedToken);
+              vm.$router.navigate(['Home'])
+            },
+            // on error
+            function(err) {
+              if (err.status === 409) vm.conflict = true;
+              $log.info('Error Claire-r:', err);
+            }
+          );
+      }
+
+      function submitLogIn() {
+        authService
+          .logIn(vm.logIn)
+          .then(
+            // on success
+            function(decodedToken) {
+              $log.info('Logged in!', decodedToken);
+              vm.$router.navigate(['Home'])
+            },
+            // on error
+            function(err) {
+              $log.info('Error:', err);
+            }
+          );
+      }
+
+      $log.info("SignInController loaded!");
     }
 })()
