863dd3f85bffe675e6a362fd93789c8628b82284
AJ Almaguer <ajalmaguer@gmail.com>
2016-05-24 17:18:06 -0700

    navbar is working with its own controller, now I need to implement the signin-signup page.

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,23 @@
 (function() {
   angular
     .module("student-portal", ["ngResource", "ngComponentRouter"])
-    .value('$routerRootComponent', 'app')
-    .component('app',{
-      templateUrl: 'js/appTemplate.html',
+    .value("$routerRootComponent", "app")
+    .component("app",{
+      templateUrl: "js/appTemplate.html",
       $routeConfig: [
         //path = url,           name= URL shortcut, component = template
-        {path: '/',             name: 'Home',       component: 'home', useAsDefault: true},
-        {path: '/listings/...', name: 'Listings',   component: 'listings'}
+        {path: "/",             name: "Home",       component: "home", useAsDefault: true},
+        {path: "/listings/...", name: "Listings",   component: "listings"},
+        {path: "/signin",       name: "Signin",     component: "signin"},
+
       ]
     })
-    .component('navbar', {
-      templateUrl: 'js/home/navbar.html',
+    .component("navbar", {
+      templateUrl: "js/home/navbar.html",
       controller: NavbarController
+    })
+    .component("signin", {
+      templateUrl: "js/auth/signin.html",
     })
 
     NavbarController.$inject = ["$log", "authService"];
