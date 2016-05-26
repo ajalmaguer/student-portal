1e3fbd0a6d1cbc4e9fc2cff1f127b498640ee3ff
AJ Almaguer <ajalmaguer@gmail.com>
2016-05-25 17:39:12 -0700

    finished creating the form.

--- public/js/listingsComponent/listings.js
+++ public/js/listingsComponent/listings.js
@@ -72,21 +72,8 @@
     var vm = this
 
     vm.states = ["AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"]
-
     vm.roomTypes = ["Private Room", "Shared Room", "Entire Place"]
     vm.propertyTypes = ["Home", "Apartment", "Loft", "Studio", "Dorm", "Other"]
-
-    vm.getA = getA
-
-    function getA(string){
-      var vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
-      if (vowels.indexOf(string[0]) === -1) {
-        return "a " + string
-      } else {
-        return "an " + string
-      }
-    }
-
     vm.newListing = {
       space:{
         accomodates: 1,
@@ -95,7 +82,8 @@
       },
       amenities: [
         {description: "Kitchen",       value:    false},
-        {description: "Washer",        value:    false},
+        {description: "Washer in unit",        value:    false},
+        {description: "Washer on property",        value:    false},
         {description: "Wifi Included", value:    false},
         {description: "Utilities Included",value: false},
         {description: "Furnished",     value:    false},
@@ -113,9 +101,32 @@
       ]
     }
 
+    vm.getA = getA
+    vm.addListing = addListing
+
+    function getA(string){
+      var vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+      if (vowels.indexOf(string[0]) === -1) {
+        return "a " + string
+      } else {
+        return "an " + string
+      }
+    }
+
+    function addListing() {
+      console.log(vm.newListing)
+    }
+
     //initilize materialize elements
     $timeout(function () {
-      $('select').material_select();
+      $('select').material_select()
+      $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year
+        onClose: function() {
+            $('.datepicker').blur();
+        }
+      })
     })
 
 
