4383c3f7eb8e30af17618c197e26db2e6cc5091d
AJ Almaguer <ajalmaguer@gmail.com>
2016-06-01 14:34:56 -0700

    added home page

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -2,6 +2,15 @@
   angular
     .module("student-portal")
     .component("home", {
-      templateUrl: "js/home/home.html"
+      templateUrl: "js/home/home.html",
+      controller: HomeController
     })
+
+  function HomeController() {
+    var vm = this
+    vm.$routerOnActivate = function (next) {
+      $('#index-banner').height(window.innerHeight)
+      $('.parallax').parallax();
+    }
+  }
 })()
