3f03496be25626890deaaaecb5926284a6be0091
AJ Almaguer <ajalmaguer@gmail.com>
2016-06-01 19:38:11 -0700

    on loading, initAutocomplete and onfocus event listener is working

--- public/js/listingsComponent/listings.js
+++ public/js/listingsComponent/listings.js
@@ -195,7 +195,8 @@
     })
 
     vm.$routerOnActivate = function () {
-      setUpAutocomplete()
+      initAutocomplete()
+      document.getElementById('autocomplete').onfocus = geolocate
     }
 
   }
@@ -315,20 +316,32 @@
     }
   }
 
-  function setUpAutocomplete() {
-    var placeSearch, autocomplete
 
-    (function initAutocomplete() {
-      // Create autocomplete object, restricting search to geographical location types.
-      autocomplete = new google.maps.places.Autocomplete(
-        (document.getElementById('autocomplete')),
-        {types: ['geocode']}
-      )
+  function initAutocomplete() {
+    // Create autocomplete object, restricting search to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+      (document.getElementById('autocomplete')),
+      {types: ['geocode']}
+    )
+
+  }
 
 
 
-    })();
-
+  function geolocate() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+        autocomplete.setBounds(circle.getBounds());
+      });
+    }
   }
 
 })()
